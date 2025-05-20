be88d41e32f36f33155196856c9f235f4149eef6
howard <howard.huang@itech99.com>
2025-05-20 11:37:14 +0800

    feat: 发布v6版本的sdk第一版

--- qx/qxKms/index.go
+++ qx/qxKms/index.go
@@ -1,9 +1,9 @@
 package qxKms
 
 import (
-	"qx-sdk-go-v6/qx/qxCtx"
-	"qx-sdk-go-v6/qx/qxKms/akc"
-	"qx-sdk-go-v6/qx/qxKms/skc"
+	"github.com/Technology-99/qx-sdk-go-v6/qx/qxCtx"
+	"github.com/Technology-99/qx-sdk-go-v6/qx/qxKms/akc"
+	"github.com/Technology-99/qx-sdk-go-v6/qx/qxKms/skc"
 )
 
 type (
